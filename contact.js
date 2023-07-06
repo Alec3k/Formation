5760c30c885b16d0048bbb37a4efd762b3abeed1
Alec ASSELINEAU <alec@imac-de-alec.home>
2023-07-06 23:24:43 +0200

    required text form

--- contact.js
+++ contact.js
@@ -1,15 +1,65 @@
-const input = document.getElementById('last-name');
 const myFormulaire = document.getElementById('my-form');
-const errorMessage = document.getElementById('error');
+
+// REQUIRED FIRST NAME
+const inputFirst = document.getElementById('first-name');
+const errorMessageFirst = document.getElementById('error-first');
 
 myFormulaire.addEventListener('submit', (e) => {
     let messages = [];
-    if (input.value === '') {
-        messages.push('Le deuxieme prenom est requis');
+    if (inputFirst.value === '') {
+        messages.push('Le premier prénom est requis');
     }
     if (messages.length > 0) {
         e.preventDefault();
-        errorMessage.innerText = messages.join(', ');
+        errorMessageFirst.innerText = messages.join(', ');
     }
+    errorMessageFirst.classList.add('error-message-active');
+});
 
+// REQUIRED LAST NAME
+const inputLast = document.getElementById('last-name');
+const errorMessageLast= document.getElementById('error-last');
+
+myFormulaire.addEventListener('submit', (e) => {
+    let messages = [];
+    if (inputLast.value === '') {
+        messages.push('Le deuxième prénom est requis');
+    }
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorMessageLast.innerText = messages.join(', ');
+    }
+    errorMessageLast.classList.add('error-message-active');
+});
+
+// REQUIRED EMAIL
+const inputMail = document.getElementById('email');
+const errorMessageMail= document.getElementById('error-mail');
+       
+myFormulaire.addEventListener('submit', (e) => {
+    let messages = [];
+    if (inputMail.value === '') {
+        messages.push('Le mail est requis');
+    }
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorMessageMail.innerText = messages.join(', ');
+    }
+    errorMessageMail.classList.add('error-message-active');
+});
+
+// REQUIRED MESSAGE
+const inputMessage = document.getElementById('message');
+const errorMessageMessage= document.getElementById('error-message');
+
+myFormulaire.addEventListener('submit', (e) => {
+    let messages = [];
+    if (inputMessage.value === '') {
+        messages.push('Le message est requis');
+    }
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorMessageMessage.innerText = messages.join(', ');
+    }
+    errorMessageMessage.classList.add('error-message-active');
 });
